2f44d71af9e5613afb4712b400f96c572de5f348
Thinh T. Diep <57164963+Thinh-T-Diep@users.noreply.github.com>
2022-09-18 15:37:39 +0700

    Delete MySQL lines

--- Server-setup-scripts/MariaDB-setup.sh
+++ Server-setup-scripts/MariaDB-setup.sh
@@ -14,13 +14,3 @@ sudo yum install -y mariadb-server
 sudo service mariadb start
 sudo systemctl enable mariadb
 sudo mysql
-
-# Create and give the privilege to the Apache server
-CREATE USER 'db_admin'@'3.82.191.129' IDENTIFIED BY 'rmit_password';
-GRANT ALL PRIVILEGES ON *.* TO 'db_admin'@'3.82.191.129';
-FLUSH PRIVILEGES;
-CREATE DATABASE rmit_store_db;
-USE rmit_store_db;
-CREATE TABLE store (id mediumint(8) unsigned NOT NULL auto_increment, Name varchar(255) default NULL, Price varchar(255) default NULL, ImageUrl varchar(255) default NULL, PRIMARY KEY (id)) AUTO_INCREMENT = 1;
-INSERT INTO store (Name, Price, ImageUrl) VALUES ("Fairtrade Pocket Hoodie", "64.95", "p-1.jpg"), ("Fairtrade Zip Hoodie", "59.95", "p-2.jpg"), ("Rudby Jergy", "69.95", "p-3.jpg"), ("Topaz Premium Zip Jacket", "34.95", "p-4.jpg"), ("Rmit Fairtrade Polo", "21.95", "p-5.jpg"), ("Portable Charger", "10", "p-6.jpg"), ("Face Mask", "8.5", "p-7.jpg"), ("Hoddie Koala", "30", "p-8.jpg"), ("Graduation Redbacks Pack", "59.95", "p-9.jpg"), ("Vinyl Pixel Keyring", "7.95", "p-10.jpg"), ("Applied Science Course Kit", "79.95", "p-11.jpg"), ("Graduation Tie", "79.99", "p-12.jpg");
-exit
