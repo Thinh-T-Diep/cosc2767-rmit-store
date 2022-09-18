a97853095952993118ee68fea324e87ddae43184
Thinh T. Diep <57164963+Thinh-T-Diep@users.noreply.github.com>
2022-09-18 16:19:39 +0700

    Change database IP

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
             <div class="row rmit-product-section">
               <?php
               $link = mysqli_connect(
-                  "35.172.118.228",
+                  "52.91.76.150",
                   "db_admin",
                   "rmit_password",
                   "rmit_store_db"
